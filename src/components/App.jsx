59f39899dc381d75b3127624b266d3ed3be87f2f
EwelinaManiecka <105506837+EwelinaManiecka@users.noreply.github.com>
2023-01-15 16:55:13 +0100

    files
    test

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,59 +1,64 @@
 import React from 'react';
-import { Component } from 'react';
+import { useState, useEffect } from 'react';
+
 import FeedbackOpinions from './Feedback/FeedbackOpinion';
 import Statistics from './Statistics/Statistic';
 import Notification from './Notification/Notification';
 
-class App extends Component {
-  state = {
-    good: 0,
-    neutral: 0,
-    bad: 0,
+// class App extends Component {
+//   state = {
+//     good: 0,
+//     neutral: 0,
+//     bad: 0,
+//   };
+
+const App = () => {
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+
+  const onLeaveFeedback = event => {
+    const name = event.target.name;
+    console.log(name);
+    name === 'good' && setGood(10 + 1);
+    name === 'neutral' && setNeutral(neutral + 1);
+    name === 'bad' && setBad(bad + 1);
+
+    console.log(good, bad, neutral);
   };
 
-  onLeaveFeedback = event => {
-    const currentBtnValue = event.currentTarget.value;
-    this.setState(prevState => ({
-      ...prevState,
-      [currentBtnValue]: prevState[currentBtnValue] + 1,
-    }));
+  useEffect(() => {
+    console.log(good, neutral, bad);
+  }, [good, neutral, bad]);
+
+  const countTotalFeedback = () => {
+    return good + bad + neutral;
   };
 
-  countTotalFeedback = () => {
-    const valuesArray = Object.values(this.state);
-    return valuesArray.reduce((acc, val) => {
-      acc += val;
-      return acc;
-    });
+  const countPositiveFeedbackPercentage = () => {
+    return Math.trunc((good / countTotalFeedback()) * 100);
   };
 
-  countPositiveFeedbackPercentage = () => {
-    return Math.trunc((this.state.good / this.countTotalFeedback()) * 100);
-  };
-
-  render() {
-    const btnNames = Object.keys(this.state);
-    const actualState = this.state;
-    return (
-      <div className="App">
-        <section>
-          <FeedbackOpinions
-            options={btnNames}
-            onLeaveFeedback={this.onLeaveFeedback}
+  const btnNames = ['good', 'bad', 'neutral'];
+  return (
+    <div className="App">
+      <section>
+        <FeedbackOpinions
+          options={btnNames}
+          onLeaveFeedback={onLeaveFeedback}
+        />
+        {countTotalFeedback() === 0 ? (
+          <Notification message="No feedback given" />
+        ) : (
+          <Statistics
+            // options={actualState}
+            total={countTotalFeedback}
+            positivePercentage={countPositiveFeedbackPercentage()}
           />
-          {this.countTotalFeedback() === 0 ? (
-            <Notification message="No feedback given" />
-          ) : (
-            <Statistics
-              options={actualState}
-              total={this.countTotalFeedback()}
-              positivePercentage={this.countPositiveFeedbackPercentage()}
-            />
-          )}
-        </section>
-      </div>
-    );
-  }
-}
+        )}
+      </section>
+    </div>
+  );
+};
 
 export default App;
--- src/components/Feedback/FeedbackOpinion.jsx
+++ src/components/Feedback/FeedbackOpinion.jsx
@@ -1,32 +1,29 @@
-import { Component } from 'react';
 import PropTypes from 'prop-types';
 import css from './FeedbackOpinion.module.css';
 
-class FeedbackOpinions extends Component {
-  render() {
-    const { options, onLeaveFeedback } = this.props;
-    return (
-      <div className={css.feedback}>
-        <h1 className={css.feedback__title}>Please leave feedback</h1>
-        <div className={css.feedback__buttons}>
-          {options.map(option => {
-            return (
-              <button
-                className={css.feedback__buttons}
-                value={option}
-                key={option}
-                type="button"
-                onClick={event => onLeaveFeedback(event)}
-              >
-                {option}
-              </button>
-            );
-          })}
-        </div>
+const FeedbackOpinions = props => {
+  const { options, onLeaveFeedback } = props;
+  return (
+    <div className={css.feedback}>
+      <h1 className={css.feedback__title}>Please leave feedback</h1>
+      <div className={css.feedback__buttons}>
+        {options.map(option => {
+          return (
+            <button
+              className={css.feedback__buttons}
+              name={option}
+              key={option}
+              type="button"
+              onClick={onLeaveFeedback}
+            >
+              {option}
+            </button>
+          );
+        })}
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
 
 FeedbackOpinions.propTypes = {
   options: PropTypes.arrayOf(PropTypes.string),
